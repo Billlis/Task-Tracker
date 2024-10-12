ea6de568f910aa24d07a8cc28a377706ac4542a9
Billlis <billlis@hotmail.com>
2024-10-12 19:23:24 +0300

    Almost done

--- functions.py
+++ functions.py
@@ -1,19 +1,19 @@
-FILEPATH = "tasks.txt"
-
+import json
+FILEPATH = "tasks.json"
 
 def get_tasks(filepath=FILEPATH):
     """ Read a text file and return the list of
     tasks.
     """
     with open(filepath, 'r') as file_local:
-        tasks_local = file_local.readlines()
+        tasks_local  = json.load(file_local)
     return tasks_local
 
 
 def write_tasks(tasks_arg, filepath=FILEPATH):
     """ Write the tasks list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(tasks_arg)
+    with open(filepath, 'w') as file_local:
+        json.dump(tasks_arg, file_local)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# from functions import get_tasks, write_tasks
 import functions
+import time
+
 
 while True:
     user_action = input("Type add, update, delete, list,(list done, list todo, list in progress) or exit: ")
@@ -9,31 +10,48 @@ while True:
         task = user_action[4:]
 
         tasks = functions.get_tasks()
-
-        tasks.append(task + '\n')
+        sum_of_tasks = len(tasks)
+        now = time.strftime("%b %d, %Y %H:%M:%S")
+        tasks[str(sum_of_tasks+1)]={"description": task, "status": "todo", "createdAt": now, "updatedAt": ""}
 
         functions.write_tasks(tasks)
-        print("Task added successfully (ID: 1)")
-        #NA GINEI ALLAGI TO ID
+        print(f"Task added successfully (ID: {sum_of_tasks+1})")
+
     elif user_action.startswith('list'):
-
         tasks = functions.get_tasks()
+        if user_action[5:]=="todo":
+            for index in tasks:
+                task=tasks[index]
+                if task["status"]=="todo":
+                    print(index, task["description"])
+        elif user_action[5:]=="in progress":
+            for index in tasks:
+                task = tasks[index]
+                if task["status"] == "in progress":
+                    print(index, task["description"])
+        elif user_action[5:]=="done":
+            for index in tasks:
+                task = tasks[index]
+                if task["status"] == "done":
+                    print(index, task["description"])
+        else:
+            for index in tasks:
+                task=tasks[index]
+                print(index, task["description"])
 
-        for index, item in enumerate(tasks):
-            item = item.strip('\n')
-            row = f"{index + 1}-{item}"
-            print(row)
     elif user_action.startswith('update'):
         try:
             number = int(user_action[7:])
             print(number)
 
-            number = number - 1
-
             tasks = functions.get_tasks()
 
             new_task = input("Enter new task: ")
-            tasks[number] = new_task + '\n'
+            task=tasks[str(number)]
+            status=task["status"]
+            createdAt=task["createdAt"]
+            now = time.strftime("%b %d, %Y %H:%M:%S")
+            tasks[str(number)] = {"description": new_task, "status": status, "createdAt": createdAt, "updatedAt": now}
 
             functions.write_tasks(tasks)
         except ValueError:
@@ -42,16 +60,17 @@ while True:
 
     elif user_action.startswith('delete'):
         try:
-            number = int(user_action[7:])
+            index = int(user_action[7:])
 
             tasks = functions.get_tasks()
-            index = number - 1
-            task_to_remove = tasks[index].strip('\n')
-            tasks.pop(index)
 
+            sum_of_tasks = len(tasks)
+            for i in range(index, sum_of_tasks):
+                tasks[str(i)]=tasks[str(i+1)]
+            tasks.pop(str(sum_of_tasks))
             functions.write_tasks(tasks)
 
-            message = f"Task {task_to_remove} was removed from the list."
+            message = f"Task {index} was removed from the list."
             print(message)
         except IndexError:
             print("There is no item with that number.")
